d13e4d4ae740362c3bc1aaf997cc3496678b56dd
Laura Skelton <lauraeskelton@gmail.com>
2014-08-11 16:20:59 -0400

    Added data manager

---
+++ datamanager.py
@@ -0,0 +1,55 @@
+import math
+import operator
+import numpy as np
+
+def loadBeerChooser(path='data'):
+    
+    # Get beer names
+    beers={}
+    for line in open(path+'/u.item'):
+        (id,name)=line.split('\t')
+        beers[id]=name.rstrip()
+    
+    # Load data
+    userPrefs={}
+    for line in open(path+'/u.data'):
+        (user,beerid,rating)=line.split(',')
+        userPrefs.setdefault(user,{})
+        userPrefs[user][beers[beerid]]=float(rating)
+    return userPrefs
+
+def transformPrefs(userPrefs):
+    itemPrefs={}
+    for beer in userPrefs:
+        for beer in userPrefs[person]:
+            itemPrefs.setdefault(beer,{})
+            # Flip person and beer
+            itemPrefs[beer][person]=userPrefs[person][beer]
+    return itemPrefs
+
+def createNDArray():
+    userPrefs = loadBeerChooser()
+    itemPrefs = transformPrefs(userPrefs)
+    beersArray = sorted(itemPrefs.iteritems(), key=operator.itemgetter(0))
+    usersArray = sorted(userPrefs.iteritems(), key=operator.itemgetter(0))
+    
+    # 2 dimensional array of zeros, each beer gets 5 zeros in the vector, one for each rating level (1...5)
+    trainingArray = np.zeros((len(usersArray), len(beersArray) * 5))
+
+     # Copy all of the ratings to the appropriate place in the vector
+    trainingResults = trainingArray
+    for j in range(0,len(usersArray)):
+        for k in range(0,len(beersArray)):
+            if beersArray[k][0] in userPrefs[usersArray[j][0]]:
+                rating = userPrefs[usersArray[j][0]][beersArray[k][0]]
+                rating = (rating * 5) - 1
+                trainingArray[j][(k * 5) + rating] = 1
+    return trainingArray
+
+
+
+
+
+
+
+
