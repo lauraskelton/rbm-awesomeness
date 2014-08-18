edb65898a09d41413397e3933f60d4827df26b57
Max <max@recoset.com>
2014-08-18 12:22:59 -0400

    refactor training

--- run_model.py
+++ run_model.py
@@ -4,49 +4,7 @@ import numpy as np
 import theano
 import theano.tensor as T
 import time
-
-def epoch(batch_size_to_use, n_train, theano_function):
-    i=0
-    costs = []
-    while i + batch_size_to_use <= n_train:
-        costs.append(theano_function(i, batch_size_to_use))
-        i += batch_size_to_use
-
-    return costs
-
-def run_epochs(training_function, batch_size, n_train, min_epochs=50, 
-                min_improvement=1.002, new_training=True, lr_decay=None, network=None):
-    if 'n' not in dir(run_epochs) or new_training:
-        run_epochs.n = 0
-
-    if 'costs' not in dir(run_epochs) or new_training:
-        run_epochs.costs = [(0, 999999)]
-    
-    start = time.time()
-
-    # train for at least this many epochs
-    epoch_stop = min_epochs
-
-    while run_epochs.n < epoch_stop:
-        run_epochs.n += 1
-        costs = epoch(batch_size, n_train, training_function)
-        print "=== epoch {} ===".format(run_epochs.n)
-        print "costs: {}".format([line[()] for line in costs])
-        print "avg: {}".format(np.mean(costs))
-        
-        # keep training as long as we are improving enough
-        if (np.mean(costs) * min_improvement) < run_epochs.costs[-1][1]:
-            epoch_stop += 1
-        elif lr_decay:
-            network.learning_rate *= lr_decay
-            print "min improvement not seen; decreasing learning rate to {}".format(network.learning_rate)
-            print "epochs left: {}".format(epoch_stop)
-
-        run_epochs.costs.append((run_epochs.n, np.mean(costs)))
-
-
-    elapsed = (time.time() - start)
-    print "ELAPSED TIME: {}".format(elapsed)
+import trainer
 
 
 #######################
@@ -78,33 +36,53 @@ shared_test_mask = theano.shared(test_mask, "test_mask")
 x = ae.matrixType('x')
 x_mask = ae.matrixType('mask')
 
-nn64_1 = ae.CFAutoencoder(data.shape[1], 64, inputs=x, mask=x_mask)
 
-nn64_2 = ae.CFAutoencoder(nn64_1.n_hidden, 32, inputs=nn64_1.active_hidden)
 
-# final output layer
-nn64_3 = ae.CFAutoencoder(nn64_2.n_hidden, data.shape[1], inputs=nn64_2.active_hidden, mask=x_mask, original_input=x)
+####################
+# TRAINING WITH WEIGHT DECAY
+
+decay_layer = ae.CFAutoencoder(data.shape[1], 12, inputs=x, mask=x_mask, weight_decay=0.0001)
+
+print "\n\t[Training] a network with weight decay!"
+
+aet = trainer.AETrainer(decay_layer, shared_train, shared_mask)
+
+aet.run_epochs()
+
+
+
+
+
+
+
+# nn64_1 = ae.CFAutoencoder(data.shape[1], 64, inputs=x, mask=x_mask)
+
+# nn64_2 = ae.CFAutoencoder(nn64_1.n_hidden, 32, inputs=nn64_1.active_hidden)
+
+# # final output layer
+# nn64_3 = ae.CFAutoencoder(nn64_2.n_hidden, data.shape[1], inputs=nn64_2.active_hidden, 
+#                             mask=x_mask, original_input=x)
 
 ######################
 # train the network
 
-i, batch_size = T.iscalars('i', 'batch_size')
 
-print "\n\t[Training] Layer 1:"
-layer1_train = theano.function([i, batch_size], nn64_1.cost, updates=nn64_1.updates,
-                                        givens={x:      shared_train[i:i+batch_size],
-                                                x_mask: shared_mask[i:i+batch_size]})
 
-run_epochs(layer1_train, 256, eighty)
+# print "\n\t[Training] Layer 1:"
+# layer1_train = theano.function([i, batch_size], nn64_1.cost, updates=nn64_1.updates,
+#                                         givens={x:      shared_train[i:i+batch_size],
+#                                                 x_mask: shared_mask[i:i+batch_size]})
 
-nn64_1.set_noise(0)
-nn64_1.learning_rate = 0.01
-print "\n\t[Tuning] Layer 1:"
-layer1_tune = theano.function([i, batch_size], nn64_1.cost, updates=nn64_1.updates,
-                                        givens={x:      shared_train[i:i+batch_size],
-                                                x_mask: shared_mask[i:i+batch_size]})
+# run_epochs(layer1_train, 256, eighty)
 
-run_epochs(layer1_tune, 256, eighty)
+# nn64_1.set_noise(0)
+# nn64_1.learning_rate = 0.01
+# print "\n\t[Tuning] Layer 1:"
+# layer1_tune = theano.function([i, batch_size], nn64_1.cost, updates=nn64_1.updates,
+#                                         givens={x:      shared_train[i:i+batch_size],
+#                                                 x_mask: shared_mask[i:i+batch_size]})
+
+# run_epochs(layer1_tune, 256, eighty)
 
 
 # print "\n\t[Training] Layer 2:"
@@ -187,7 +165,7 @@ run_epochs(layer1_tune, 256, eighty)
 # save mask of how data was corrupted, as well as mask of missing/meaningless input data
 # again, we already did this in nn64... it is nn64_1.noise.
 # but wait! we called set_noise(0) above, so we need to reset the noise to re-corrupt the inputs.
-nn64_1.set_noise(0.5)
+# nn64_1.set_noise(0.5)
 
 # run it through the network to the first hidden layer
 # didn't we already do this as well with the exact same data? hmm... this should be nn64_1.active_hidden (= the activations of the first hidden layer)
@@ -203,42 +181,29 @@ nn64_1.set_noise(0.5)
 # nn64_3.active_hidden should be the predictions of the input data after going through our neural network
 
 # mask out the meaningless data from the neural network's output matrix
-# mask out the values we input (the uncorrupted values) from the output matrix (so we are only measuring error on beers that were predicted)
-nn64_prediction = T.dot(T.dot(x_mask, T.transpose(nn64_3.active_hidden)), 1 - nn64_1.noise)
+# # mask out the values we input (the uncorrupted values) from the output matrix (so we are only measuring error on beers that were predicted)
+# nn64_prediction = T.dot(T.dot(x_mask, T.transpose(nn64_3.active_hidden)), 1 - nn64_1.noise)
 
-# use root mean squared error to check how accurate our predictions were when using the entire neural network
-nn64_test_error = T.mean(T.pow(T.mean(T.pow(nn64_prediction - nn64_1.inputs, 2)), 0.5))
-
-
+# # use root mean squared error to check how accurate our predictions were when using the entire neural network
+# nn64_test_error = T.mean(T.pow(T.mean(T.pow(nn64_prediction - nn64_1.inputs, 2)), 0.5))
 
 
-layer1_pred = T.dot(T.dot(x_mask, T.transpose(nn64_1.output)), 1-nn64_1.noise)
+
+
+# layer1_pred = T.dot(T.dot(x_mask, T.transpose(nn64_1.output)), 1-nn64_1.noise)
 
 
 
 ######## How do I actually call the testing function with Theano?
 
-n64_testing_function = theano.function([x,x_mask], nn64_test_error)
+# n64_testing_function = theano.function([x,x_mask], nn64_test_error)
 
-cost = []
-for i in xrange(0,10):
-    cost.append(n64_testing_function(shared_train.get_value(), shared_mask.get_value()))
+# cost = []
+# for i in xrange(0,10):
+#     cost.append(n64_testing_function(shared_train.get_value(), shared_mask.get_value()))
 
-print "\n\t[Testing] 64-hidden node autoencoder error: {}".format(T.mean(cost))
-
-
+# print "\n\t[Testing] 64-hidden node autoencoder error: {}".format(T.mean(cost))
 
 
-####################
-# TRAINING WITH WEIGHT DECAY
 
-decay_layer = ae.CFAutoencoder(data.shape[1], 12, inputs=x, mask=x_mask, weight_decay=0.0001)
 
-i, batch_size = T.iscalars('i', 'batch_size')
-
-print "\n\t[Training] a network with weight decay!"
-decay_training = theano.function([i, batch_size], decay_layer.cost, updates=decay_layer.updates,
-                                        givens={x:      shared_train[i:i+batch_size],
-                                                x_mask: shared_mask[i:i+batch_size]})
-run_epochs(decay_training, 256, eighty, min_epochs=200, min_improvement=1.001, 
-                lr_decay=0.99, network=decay_layer)
---
+++ trainer.py
@@ -0,0 +1,67 @@
+import theano
+import theano.tensor as T
+import numpy as np
+import time
+
+def epoch(batch_size_to_use, n_train, theano_function):
+	i=0
+	costs = []
+	while i + batch_size_to_use <= n_train:
+		costs.append(theano_function(i, batch_size_to_use))
+		i += batch_size_to_use
+
+	return costs
+
+
+
+class AETrainer(object):
+	def __init__(self, model, inputs, mask=None, batch_size=64):
+		self.i, self.bs = T.iscalars('i', 'bs')
+		self.model = model
+		self.inputs = inputs
+		self.batch_size = batch_size
+
+		self.steps = 0
+		self.costs = []
+
+		self.costfunc = model.cost
+		self.updates = model.updates
+
+	def get_training_function(self, cost, updates):
+		given = {model.inputs : self.inputs[self.i:self.i+self.bs]}
+
+		if model.mask:
+			given[model.mask] = self.mask[self.i:self.i+self.bs]
+
+		return theano.function([self.i, self.bs], cost, updates, givens=given)
+
+	def run_epochs(self, min_epochs=50, min_improvement=1.001, 
+					lr_decay=0.0, decay_modulo=25):
+		start = time.time()
+		training_function = self.get_training_function(self.costfunc, self.updates)
+
+		# train for at least this many epochs
+		epoch_stop = min_epochs
+
+		while self.steps < epoch_stop:
+			self.steps += 1
+			costs = epoch(batch_size, n_train, training_function)
+			print "=== epoch {} ===".format(self.steps)
+			print "costs: {}".format([line[()] for line in costs])
+			print "avg: {}".format(np.mean(costs))
+			
+			# keep training as long as we are improving enough
+			if (np.mean(costs) * min_improvement) < self.costs[-1][1]:
+				epoch_stop += 1
+			elif lr_decay and self.steps % decay_modulo == 0:
+				self.model.learning_rate *= lr_decay
+				print "min improvement not seen; decreasing learning rate to {}".format(network.learning_rate)
+				print "epochs left: {}".format(epoch_stop)
+				# recompile training function with new learning rate
+				training_function = self.get_training_function(self.costfunc, self.updates)
+
+			self.costs.append((self.steps, np.mean(costs)))
+
+
+		elapsed = (time.time() - start)
+		print "ELAPSED TIME: {}".format(elapsed)
